f7530a5ca8d34681c6e7c56573be74f430b2be21
Wanu23 <jruizwanumen@gmail.com>
2025-05-26 07:20:25 -0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBarDetalle() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">Adóptame</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBarDetalle;
 
